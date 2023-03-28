397e6b5e96c583e9cf6d1c5d496756e17b2e0cd6
MukengeNJ <john.mukenge>
2023-03-28 07:56:51 +0200

    ExploreTopBooks component

---
+++ 03-frontend/librairia-app/src/layouts/HomePage/ExploreTopBooks.tsx
@@ -0,0 +1,13 @@
+export const ExploreTopBooks = () => {
+    return (
+        <div className="p-5 mb-4 bg-dark header">
+            <div className="container-fluid py-5 text-white d-flex justify-content-center align-items-center">
+                <div>
+                    <h1 className="display-5 fw-bold">Trova la tua prossima avventura</h1>
+                    <p className="col-md-8 fs-">Dove vogliamo andare la prossima volta?</p>
+                    <a type="button" className="btn main-color btn-lg text-white" href="#">Scopri i libri</a>
+                </div>
+            </div>
+        </div>
+    )
+}
