3befc0efbee7ff29bf331b120b42273b20a8b647
MukengeNJ <john.mukenge>
2023-03-29 12:34:33 +0200

    added for starting backend

---
+++ 02-backend/springbootlibrairia/src/main/java/it/librairia/springbootlibrairia/SpringbootlibrairiaApplication.java
@@ -0,0 +1,13 @@
+package it.librairia.springbootlibrairia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootlibrairiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootlibrairiaApplication.class, args);
+	}
+
+}
---
+++ 02-backend/springbootlibrairia/src/test/java/it/librairia/springbootlibrairia/SpringbootlibrairiaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.librairia.springbootlibrairia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootlibrairiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
