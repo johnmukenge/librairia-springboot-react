73ba2742dd448fb7e599036d73a55fb3c9c70a3c
MukengeNJ <john.mukenge>
2023-03-29 20:19:16 +0200

    useEffect

--- 03-frontend/librairia-app/src/layouts/HomePage/components/Carousel.tsx
+++ 03-frontend/librairia-app/src/layouts/HomePage/components/Carousel.tsx
@@ -1,5 +1,52 @@
 import { ReturnBook } from "./ReturnBook";
+import { useEffect, useState } from "react";
+import BookModel from "../../../models/BookModel";
+
 export const Carousel = () => {
+
+    const [books, setBooks] = useState<BookModel[]>([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [httpError, setHttpError] = useState(null);
+
+    useEffect(() => {
+        const fetchBooks = async () => {
+            // create api to fetch all the api
+            const baseUrl: string = "http://localhost:8080/api/books";
+
+            const url: string = `${baseUrl}?page=0&size=9`;
+
+            const response = await fetch(url);
+
+            if (!response.ok) {
+                throw new Error('Qualcosa è andata storto!');
+            }
+
+            const responseJson = await response.json();
+
+            const responseData = responseJson._embedded.books;
+
+            const loadedBooks: BookModel[] = [];
+
+            for (const key in responseData) {
+                loadedBooks.push({
+                    id: responseData[key].id,
+                    title: responseData[key].title,
+                    author: responseData[key].author,
+                    description: responseData[key].description,
+                    copies: responseData[key].copies,
+                    copiesAvailable: responseData[key].copiesAvailable,
+                    img: responseData[key].img
+                });
+            }
+            setBooks(loadedBooks);
+            setIsLoading(false);
+        };
+        fetchBooks().catch((error: any) => {
+            setIsLoading(false);
+            setHttpError(error.message);
+        })
+    }, []);// will be called everytime something change inside this array
+
     return (
         <div className="container mt-5" style={{ height: 550 }}>
             <div className="homepage-carousel-title">
