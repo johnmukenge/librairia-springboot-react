02decde9428456c56f30bb962c77a38d383ef67c
MukengeNJ <john.mukenge>
2023-03-29 20:41:46 +0200

    ReturnBook props

--- 03-frontend/librairia-app/src/layouts/HomePage/components/ReturnBook.tsx
+++ 03-frontend/librairia-app/src/layouts/HomePage/components/ReturnBook.tsx
@@ -1,16 +1,26 @@
 import React from "react";
+import BookModel from "../../../models/BookModel";
 
-export const ReturnBook = () => {
+export const ReturnBook: React.FC<{ book: BookModel }> = (props) => {
     return (
         <div className="col-xs-6 col-sm-6 col-md-4 col-lg-3 mb-3">
             <div className="text-center">
-                <img src={require('./../../../Images/BooksImages/book-luv2code-1000.png')}
-                    height='233'
-                    width='151'
-                    alt=""
-                />
-                <h6 className="mt-2">Libro</h6>
-                <p>AmoLeggere</p>
+                {props.book.img ?
+                    <img src={props.book.img}
+                        height='233'
+                        width='151'
+                        alt=""
+                    />
+                    :
+                    <img src={require('./../../../Images/BooksImages/book-luv2code-1000.png')}
+                        height='233'
+                        width='151'
+                        alt=""
+                    />
+                }
+
+                <h6 className="mt-2">{props.book.title}</h6>
+                <p>{props.book.author}</p>
                 <a className="btn main-color text-white" href="#">Riservato</a>
             </div>
         </div>
