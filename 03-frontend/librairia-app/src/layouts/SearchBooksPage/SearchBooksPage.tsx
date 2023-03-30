a4b281ebef92a68136d791f27b159c4fcf447cbc
MukengeNJ <john.mukenge>
2023-03-30 22:36:05 +0200

    React: find by category

--- 03-frontend/librairia-app/src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ 03-frontend/librairia-app/src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -15,6 +15,7 @@ export const SearchBooksPage = () => {
     const [totalPages, setTotalPages] = useState(0);
     const [search, setSearch] = useState('');
     const [searchUrl, setSearchUrl] = useState('');
+    const [categorySelection, setCategorySelection] = useState('Book category');
 
     useEffect(() => {
         const fetchBooks = async () => {
@@ -87,6 +88,21 @@ export const SearchBooksPage = () => {
         }
     }
 
+    const categoryField = (value: string) => {
+        if (
+            value.toLocaleLowerCase() === 'fe' ||
+            value.toLocaleLowerCase() === 'be' ||
+            value.toLocaleLowerCase() === 'data' ||
+            value.toLocaleLowerCase() === 'devops'
+        ) {
+            setCategorySelection(value);
+            setSearchUrl(`/search/findByCategory?category=${value}&page=0&size=${booksPerPage}`);
+        } else {
+            setCategorySelection('All');
+            setSearchUrl(`?page=0&size=${booksPerPage}`);
+        }
+    }
+
     const indexOfLastBook: number = currentPage * booksPerPage;
     const indexOfFirstBook: number = indexOfLastBook - booksPerPage;
     let lastItem = booksPerPage * currentPage <= totalAmountOfBooks
@@ -122,22 +138,22 @@ export const SearchBooksPage = () => {
                                     data-bs-toggle='dropdown'
                                     aria-expanded='false'
                                 >
-                                    Category
+                                    {categorySelection}
                                 </button>
                                 <ul className="dropdown-menu" aria-labelledby='dropdownMenuButton1'>
-                                    <li>
+                                    <li onClick={() => categoryField('All')}>
                                         <a href="#" className="dropdown-item">All</a>
                                     </li>
-                                    <li>
+                                    <li onClick={() => categoryField('FE')}>
                                         <a href="#" className="dropdown-item">Front end</a>
                                     </li>
-                                    <li>
+                                    <li onClick={() => categoryField('BE')}>
                                         <a href="#" className="dropdown-item">Back End</a>
                                     </li>
-                                    <li>
+                                    <li onClick={() => categoryField('Data')}>
                                         <a href="#" className="dropdown-item">Data</a>
                                     </li>
-                                    <li>
+                                    <li onClick={() => categoryField('DevOps')}>
                                         <a href="#" className="dropdown-item">DevOps</a>
                                     </li>
                                 </ul>
